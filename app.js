7b209117bd70ff77a222c6aa16a8038f029296b3
AswaUmino <ashishmishrajune2000@gmail.com>
2020-10-21 12:20:48 +0530

    Added Add Remove  Function

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
 const getNotes = require("./notes.js")
 const validator = require("validator")
-const note = getNotes()
-console.log(note)
+const chalk = require("chalk")
+const yargs = require("yargs")
 
-console.log("hello World")
-console.log(validator.isEmail("name@example.com"))
+yargs.command({
+    command: "add",
+    describe: "Add a note",
+    builder: {
+        title:{
+            describe: "Note Title",
+            demandOption: true,
+            type: "string"
+        },
+        body:{
+            describe: "Note Body",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function (argv) {
+        console.log(chalk.green("Title: "+argv.title))
+        console.log(chalk.green("Body: "+argv.body))
+    }
+})
+
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function () {
+        console.log(chalk.red("Removing Note!"))
+    }
+})
+
+yargs.command({
+    command: "list",
+    describe: "List notes",
+    handler: function () {
+        console.log(chalk.yellow("Listing Note!"))
+    }
+})
+
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: function () {
+        console.log(chalk.blue("Reading Note!"))
+    }
+})
+
+
+yargs.argv
